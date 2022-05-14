9c8ebbe0cc3ec4ab6440e3ce0b0f95d0df0942cd
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2022-05-14 09:24:19 +0200

    Time: 1 ms (98.83%), Space: 44.7 MB (14.21%) - LeetHub

---
+++ 443-string-compression/443-string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;
+        for(int i =0; i < chars.length; i++)
+        {
+            int count = 1;
+            while(i < chars.length - 1 && chars[i] == chars[i+1])
+            {
+                i++;
+                count++;
+            }
+            chars[index] = chars[i];
+            index++;
+            if (count > 1) 
+            {
+                for (char c : Integer.toString(count).toCharArray()) 
+                {
+                     chars[index] = c;
+                     index++;
+                }
+            }
+        }
+    return index;
+    }
+}
