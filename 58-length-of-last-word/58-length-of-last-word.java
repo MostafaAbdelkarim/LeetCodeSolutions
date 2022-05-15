4d183591f3a395a3ccccc873b458f5e4fc1a5553
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2022-05-15 11:31:27 +0200

    Time: 0 ms (100.00%), Space: 42.4 MB (40.06%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int lastWordLength = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ' && lastWordLength > 0) {
+                return lastWordLength;
+            } else if (s.charAt(i) != ' ') {
+                lastWordLength++;
+            }
+        }
+        return lastWordLength;
+    }
+}
