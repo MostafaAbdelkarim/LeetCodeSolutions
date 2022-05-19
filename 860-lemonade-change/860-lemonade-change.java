598217ae517bcc27ba9f4a4d11256b83b90e8e0f
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2022-05-19 14:17:24 +0300

    Time: 1 ms (100.00%), Space: 53.9 MB (84.00%) - LeetHub

---
+++ 860-lemonade-change/860-lemonade-change.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int fives = 0;
+        int tens = 0;
+        for(int bill : bills){
+            switch(bill){
+                case 5:
+                    fives++;
+                    break;
+                case 10:
+                    fives--;
+                    tens++;
+                    if(fives < 0){
+                        return false;
+                    }
+                    break;
+                case 20:
+                    if(tens > 0 && fives > 0){
+                        tens--;
+                        fives--;
+                        break;
+                    } else if(fives > 2){
+                        fives -= 3;
+                        break;
+                    }
+                    return false;
+            }
+        }
+        return true;
+    }
+}
