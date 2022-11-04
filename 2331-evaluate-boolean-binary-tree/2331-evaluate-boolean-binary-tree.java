6dd28f38e0ac7ac744e2a1f79a5ba74b2730e83b
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2022-11-04 17:22:09 +0200

    Time: 0 ms (100.00%), Space: 46.5 MB (42.65%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -15,13 +15,8 @@
  */
 class Solution {
     public boolean evaluateTree(TreeNode root) {
-        if(root.val == 0) 
-        {
-            return false;
-        }
-        if(root.val == 1){
-            return true;
-        }
+        if(root.val == 0) return false;
+        if(root.val == 1) return true;
         if(root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
         else return evaluateTree(root.left) && evaluateTree(root.right);
     }
