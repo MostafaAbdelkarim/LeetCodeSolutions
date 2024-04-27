aabab5403533859c8a89cec0fb85b39209582e5d
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2024-04-27 21:15:41 +0300

    Time: 6 ms (31.80%), Space: 44.7 MB (46.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        final Map<Character, Integer> romanMap = romanMap();
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
+                result -= romanMap.get(s.charAt(i));
+            } else {
+                result += romanMap.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+    
+    private Map<Character, Integer> romanMap() {
+        final Map<Character, Integer> result = new HashMap<>();
+        result.put('I', 1);
+        result.put('V', 5);
+        result.put('X', 10);
+        result.put('L', 50);
+        result.put('C', 100);
+        result.put('D', 500);
+        result.put('M', 1000);
+        return result;
+    }
+}
