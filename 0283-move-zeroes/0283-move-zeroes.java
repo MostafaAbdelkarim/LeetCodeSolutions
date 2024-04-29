02ff57fe300cad850f9e7ae85ddcb9712de7b478
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2024-04-29 22:50:11 +0300

    Time: 2 ms (86.23%), Space: 45.7 MB (86.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(nums[i]==0){
-                for(int j=i;j<n-1;j++){
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
-                n--;
-            }
-            if(nums[i]==0){
-                i--;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
         }
-    }
+     }
 }
