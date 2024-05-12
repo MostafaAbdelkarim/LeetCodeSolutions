7ab30c7ee1a6a8fc7e2ae98623173569cf862b46
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2024-05-12 22:44:44 +0300

    Time: 1 ms (100.00%), Space: 44.7 MB (50.45%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int n = num.length();
+        while (num.charAt(n - 1) == '0') n--;
+        return num.substring(0, n); 
+    }
+}
