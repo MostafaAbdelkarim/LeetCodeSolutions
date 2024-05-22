619c9874edc26e6c8d4c3bd5fa840119724e55ed
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2024-05-22 14:30:47 +0300

    Time: 14 ms (34.23%), Space: 45.3 MB (49.63%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,15 +1,36 @@
 class Solution {
-    public int minFlipsMonoIncr(String s) {
-        int flipCount = 0;
-        int countOnes = 0;
+    
+    // Initial solution
+//     int flipCount = 0;
+//         int countOnes = 0;
 
-        for (char c : s.toCharArray()) {
-            if (c == '1') {
-                countOnes++;
-            } else {
-                flipCount = Math.min(flipCount + 1, countOnes);
-            }
+//         for (char c : s.toCharArray()) {
+//             if (c == '1') {
+//                 countOnes++;
+//             } else {
+//                 flipCount = Math.min(flipCount + 1, countOnes);
+//             }
+//         }
+//         return flipCount;
+    
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        final int[] prefixOnes = new int[n + 1];
+        final int[] suffixZeros = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            prefixOnes[i + 1] = prefixOnes[i] + (s.charAt(i) == '1' ? 1 : 0);
         }
-        return flipCount;
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffixZeros[i] = suffixZeros[i + 1] + (s.charAt(i) == '0' ? 1 : 0);
+        }
+
+        int minFlips = Integer.MAX_VALUE;
+        for (int i = 0; i <= n; i++) {
+            minFlips = Math.min(minFlips, prefixOnes[i] + suffixZeros[i]);
+        }
+
+        return minFlips;
     }
 }
