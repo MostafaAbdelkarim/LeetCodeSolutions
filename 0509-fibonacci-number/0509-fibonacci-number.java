2eb796fb2b7049de3b3d7770bde682b64228abc4
Mostafa ElDahshan <56657502+MostafaAbdelkarim@users.noreply.github.com>
2024-05-28 15:21:05 +0300

    Time: 0 ms (100.00%), Space: 40 MB (82.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,25 @@
 class Solution {
-    // using DP approach (Recursion + Memoization)
-    private final Map<Integer, Integer> resultMap = new HashMap<>();
+    // using DP Buttom-up approach
     public int fib(int n) {
         if (n <= 1) return n;
-        if (resultMap.containsKey(n)) {
-            return resultMap.get(n);
-        } else {
-            resultMap.put(n, fib(n-1) + fib(n-2));
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i - 2];
         }
-        return resultMap.get(n);
+        return dp[n];
     }
 }
+
+// using DP approach
+// private final Map<Integer, Integer> resultMap = new HashMap<>();
+//     public int fib(int n) {
+//         if (n <= 1) return n;
+//         if (resultMap.containsKey(n)) {
+//             return resultMap.get(n);
+//         } else {
+//             resultMap.put(n, fib(n-1) + fib(n-2));
+//         }
+//         return resultMap.get(n);
+//     }
